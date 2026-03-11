abeadbcbdf9d7ad941f9890d2e42022212efa752
ornob011 <ornob011@gmail.com>
2026-03-11 10:39:48 +0600

    25-project-run: Refine prompt templates and remove unused import in BaseEntityExtractor

--- rfp-service/src/main/java/com/dsi/rfp/adapter/entity/BaseEntityExtractor.java
+++ rfp-service/src/main/java/com/dsi/rfp/adapter/entity/BaseEntityExtractor.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StreamUtils;
 
